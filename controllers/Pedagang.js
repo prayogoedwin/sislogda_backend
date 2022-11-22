9af0752dd99db98d899935a24b12b4fd974d4e36
Edwin <you@example.comprayogo.edwin@gmail.com>
2022-11-22 16:35:30 +0700

    komditas pedagang all

--- controllers/Pedagang.js
+++ controllers/Pedagang.js
@@ -3,6 +3,7 @@ import Pedagangs from "../models/PedagangModel.js";
 import Kabkotas from "../models/KabkotaModel.js";
 import Kecamatans from "../models/KecamatanModel.js";
 import Kelurahans from "../models/KelurahanModel.js";
+import Komoditass from "../models/KomoditasModel.js";
 import {Op} from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
@@ -21,6 +22,11 @@ export const getPedagang = async(req, res) =>{
      Pedagangs.belongsTo(Kelurahans, {
         targetKey:'id',
         foreignKey: 'kelurahan_id'
+     });
+
+     Pedagangs.belongsTo(Komoditass, {
+        targetKey:'id',
+        foreignKey: 'komoditas'
      });
 
     const page = parseInt(req.query.page) || 0;
@@ -48,6 +54,10 @@ export const getPedagang = async(req, res) =>{
         },
         {
             model: Kelurahans,
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }
+        },
+        {
+            model: Komoditass,
             attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }
         }
         ],
