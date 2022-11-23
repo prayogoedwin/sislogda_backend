49a1aba05a8d188e966b19f691a58a1be0208295
Edwin <you@example.comprayogo.edwin@gmail.com>
2022-11-23 17:19:31 +0700

    kabkota update

--- controllers/Kabkota.js
+++ controllers/Kabkota.js
@@ -44,32 +44,72 @@ export const getKabkotas = async (req, res) => {
 }
 
 export const getKabKotasByProv = async (req, res) => {
-    try {
-        const kabkota = await Kabkotas.findAll({
-            where: {
-                province_id: req.query.province_id,
-                deletedAt: null
-            },
-            attributes: { exclude: ['createdAt','updatedAt', 'deletedAt'] }
-        });
+    var kabkota_id = req.query.kabkota_id;
+    if(kabkota_id != ''){
 
-        if(kabkota.length > 0){
-            res.statusCode = 200;
+        try {
+            const kabkota = await Kabkotas.findAll({
+                
+                where: {
+                    id: req.query.kabkota_id,
+                    province_id: req.query.province_id,
+                    deletedAt: null
+                },
+                attributes: { exclude: ['createdAt','updatedAt', 'deletedAt'] }
+            });
+    
+            if(kabkota.length > 0){
+                res.statusCode = 200;
+                res.json({
+                    'status' : 1,
+                    'message': 'Data berhasil ditemukan',
+                    // 'data': user[0]['name'],
+                    'data' : kabkota,
+                    // 'data' : dt
+                });
+            }
+        } catch (err){
+            // console.log(err);
+            res.statusCode = 404;
             res.json({
-                'status' : 1,
-                'message': 'Data berhasil ditemukan',
-                // 'data': user[0]['name'],
-                'data' : kabkota,
-                // 'data' : dt
+                'status' : 0,
+                // 'message': err['errors'][0]['message']
+                'message': 'Error'
             });
         }
-    } catch (err){
-        // console.log(err);
-        res.statusCode = 404;
-        res.json({
-            'status' : 0,
-            // 'message': err['errors'][0]['message']
-            'message': 'Error'
-        });
+        
+    }else{
+
+        try {
+            const kabkota = await Kabkotas.findAll({
+                
+                where: {
+                    province_id: req.query.province_id,
+                    deletedAt: null
+                },
+                attributes: { exclude: ['createdAt','updatedAt', 'deletedAt'] }
+            });
+    
+            if(kabkota.length > 0){
+                res.statusCode = 200;
+                res.json({
+                    'status' : 1,
+                    'message': 'Data berhasil ditemukan',
+                    // 'data': user[0]['name'],
+                    'data' : kabkota,
+                    // 'data' : dt
+                });
+            }
+        } catch (err){
+            // console.log(err);
+            res.statusCode = 404;
+            res.json({
+                'status' : 0,
+                // 'message': err['errors'][0]['message']
+                'message': 'Error'
+            });
+        }
+
     }
+    
 }
